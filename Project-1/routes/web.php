6394a87297b280439e044f27775d43ac66da1792
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-23 03:50:49 +0600

    Update

--- Project-1/routes/web.php
+++ Project-1/routes/web.php
@@ -11,8 +11,23 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/greeting', function () {
+    return 'Hello World';
 });
+
+Route::get('/user/{id}/profile', function (string $id) {
+    return '<h1>My Profile</h1>';
+})->name('profile');
+
+Route::get('/user/{id}/profile', function (string $id) {
+    return "User ID: $id | Photos: " . request('photos');
+})->name('profile');
+
+// $url = route('profile', ['id' => 1, 'photos' => 'yes']);
+
+Route::get('/about', function () {
+    return '<a href="' . route('profile', ['id' => 1]) . '">My Profile</a>';
+})->name('about');
+// $url = route('profile', ['id' => 1]);
